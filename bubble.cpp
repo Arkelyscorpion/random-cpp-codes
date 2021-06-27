abf00bfa63d9284c943ee962dbf025bf17169b7e
Krishna Sameer <39343961+Arkelyscorpion@users.noreply.github.com>
2021-06-27 16:31:29 +0530

    Bubble Sort Program added

---
+++ bubble.cpp
@@ -0,0 +1,41 @@
+//CREATED : 5/31/2021, 9:48:56 AM
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define pb push_back
+#define X first
+#define Y second
+#define mod 10000000007
+using namespace std::chrono;
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+void bubble(int arr[],int n){
+    int i, j;
+    for (i = 0; i < n-1; i++){
+    for (j = 0; j < n-i-1; j++){
+        if (arr[j] > arr[j+1]) // 1 3 2
+        swap(&arr[j], &arr[j+1]);} // 3 2 1 -- > 1 2 3
+    }
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i = 0 ;i<n;i++){
+        cin >> arr[i];
+    }
+    auto start = high_resolution_clock::now();
+    bubble(arr,n);
+    for(int i = 0;i<n;i++)
+    cout << arr[i] << " ";
+    auto stop = high_resolution_clock::now();
+  	auto duration = duration_cast<nanoseconds>(stop - start);
+  	std::cout << duration.count() << "\n";
+    return 0;
+}
---
+++ maxsub.cpp
@@ -0,0 +1,29 @@
+//CREATED : 6/4/2021, 3:02:55 PM
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define pb push_back
+#define X first
+#define Y second
+#define mod 10000000007
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    vector<int> v;
+    vector<int> msums;
+    vector<int> sums;
+    int n;
+    cin >> n;
+    for(int i = 0;i<n;i++){
+        int a;
+        cin >> a;
+        v.pb(a);
+    }
+    for(int i = 2;i<n;i++){
+        for(int j = 0;j<n;j++){
+                
+        }
+    }
+
+    return 0;
+}
