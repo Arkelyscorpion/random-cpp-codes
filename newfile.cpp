142d3517d943d15585e334acbef084b4a9317c83
Krishna Sameer <39343961+Arkelyscorpion@users.noreply.github.com>
2021-06-28 12:06:30 +0530

    ADDED IGNORE

---
+++ newfile.cpp
@@ -0,0 +1,16 @@
+//
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define pb push_back
+#define X first
+#define Y second
+#define mod 1000000007
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int a;
+    cin >> a;
+    
+    return 0;
+  }
